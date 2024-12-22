8570661776bdbebc816ecbd67a0fbeb9e70f72d9
mihamik603 <vmm1331@gmail.com>
2024-12-22 21:40:39 +0400

    Update module_16_4.py

--- module_16_4.py
+++ module_16_4.py
@@ -4,38 +4,26 @@ from typing import List, Optional
 
 app = FastAPI()
 
-# Модель пользователя
 class User(BaseModel):
     id: int
     username: str
     age: int
 
-# Изначальный пустой список пользователей
 users: List[User ] = []
 
-# GET запрос для получения всех пользователей
 @app.get("/users", response_model=List[User ])
 async def get_users():
     return users
 
-# POST запрос для добавления нового пользователя
 @app.post("/user/{username}/{age}", response_model=User )
-async def create_user(
-    username: str,
-    age: int
-):
+async def create_user(username: str,age: int):
     new_id = (users[-1].id + 1) if users else 1
     new_user = User(id=new_id, username=username, age=age)
     users.append(new_user)
     return new_user
 
-# PUT запрос для обновления пользователя
 @app.put("/user/{user_id}", response_model=User )
-async def update_user(
-    user_id: int = Path(..., description="Enter user ID"),
-    username: str = Path(..., description="Enter username"),
-    age: int = Path(..., description="Enter age")
-):
+async def update_user(user_id: int = Path(..., description="Enter user ID"),username: str = Path(..., description="Enter username"),age: int = Path(..., description="Enter age")):
     for user in users:
         if user.id == user_id:
             user.username = username
@@ -43,7 +31,6 @@ async def update_user(
             return user
     raise HTTPException(status_code=404, detail="User  was not found")
 
-# DELETE запрос для удаления пользователя
 @app.delete("/user/{user_id}", response_model=User )
 async def delete_user(user_id: int = Path(..., description="Enter user ID")):
     for index, user in enumerate(users):
